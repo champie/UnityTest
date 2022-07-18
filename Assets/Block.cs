0ce3be426b5f977ef96dad504115fb71e3e85536
Richard Horrocks <richard@champie.org>
2022-07-18 12:25:34 +0100

    Changed block colour

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -7,7 +7,7 @@ public class Block : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
     }
 
     // Update is called once per frame
